a93541b022e6c8079e4d9c84881d0c4d1bb08596
Jane Nogueira <jane.tecnicavocal@gmail.com>
2022-01-30 22:42:54 -0300

    Abstraindo classes POO com jogo RPG

---
+++ PROJ.jogorpg/JogoRPG/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using JogoRPG.src.Entities;
+
+namespace dotnet_poo
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Arus hero = new Arus("Arus", 23, "Knight");
+            Arus oponnet = new Arus("Maleficus", 99, "Devil");
+            Arus wizard = new Arus("Jennica", 25, "White Wizard");
+       
+            Console.WriteLine(wizard.Attack());
+            
+        }
+    }
+}
+
---
+++ PROJ.jogorpg/JogoRPG/src/Entities/Arus.cs
@@ -0,0 +1,21 @@
+namespace JogoRPG.src.Entities
+{
+    public class Arus
+    {
+        public Arus(string Name, int Level, string HeroType)
+        {
+            this.Name = Name;
+            this.Level = Level;
+            this.HeroType = HeroType;
+        }
+        public string Name;
+        public int Level;
+        public string HeroType;
+
+
+        public override string ToString()
+        {
+            return this.Name + " " + this.Level + " " + this.HeroType;
+        }
+    }
+}
